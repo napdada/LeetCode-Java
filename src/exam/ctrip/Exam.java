a2983a50005ec001240711efbb09983afc474cd7
PAN <37497838+panda-pla@users.noreply.github.com>
2021-04-02 15:34:16 +0800

    携程笔试

---
+++ src/exam/ctrip/Exam.java
@@ -0,0 +1,64 @@
+package exam.ctrip;
+
+import java.util.Scanner;
+
+/**
+ * 输出 SQLite 语句中的表名
+ */
+public class Exam {
+
+    public static void main(String[] args) {
+
+        String s = "";
+        Scanner input = new Scanner(System.in);
+        while (!input.nextLine().isEmpty()) {
+            s += input.nextLine();
+            input.nextLine();
+            if (s == null) break;
+        }
+        String[] word = s.split(" ");
+
+        for (int i = 0; i < word.length - 1; i++) {
+            boolean flag1 = word[i].equals("from") || word[i].equals("join")
+                    || word[i].equals("update") || word[i].equals("delete")
+                    || word[i].equals("into");
+            if (flag1 && !word[i + 1].equals("(")) System.out.println(word[i + 1]);
+        }
+
+//        String s = "select t.id, t.name, t.tag_id\n" +
+//                "from (\n" +
+//                "    select user.id, user.name, tag.tag_id\n" +
+//                "    from user\n" +
+//                "    inner join user_tag\n" +
+//                ") t";
+//        String[] line = s.split("\n");
+//        String[] word = s.split("\n");
+//        for (int i = 0; i < line.length; i++) {
+//            line[i].split(" ");
+//        }
+//        Scanner input = new Scanner(System.in);
+//        StringBuilder str = new StringBuilder();
+//        String tmp = null;
+//        while (true) {
+//            str.append(tmp);
+//            str.append(input.nextLine());
+//            System.out.println("cnm");
+//            // System.out.println(Arrays.toString(input.nextLine().split(" ")));
+//            tmp = input.nextLine();
+//            if (tmp == null) break;
+//        }
+//        System.out.println("str = " + str);
+//        for (int i = 0; i < word.length; i++) {
+//            System.out.println(word[i]);
+//        }
+//
+//        System.out.println("---------------");
+//
+//        for (int i = 0; i < word.length - 1; i++) {
+//            boolean flag1 = word[i].equals("from") || word[i].equals("join")
+//                    || word[i].equals("update") || word[i].equals("delete")
+//                    || word[i].equals("into");
+//            if (flag1 && !word[i + 1].equals("(")) System.out.println(word[i + 1]);
+//        }
+    }
+}
---
+++ src/exam/ctrip/Exam2.java
@@ -0,0 +1,60 @@
+package exam.ctrip;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * 输出满足条件的组合的最低花费
+ */
+public class Exam2 {
+    public static void main(String[] args) {
+//        int n = 3;
+//        String[] packages = {"1,2", "2,3", "1,3"};
+//        int[] price = {3, 4, 2};
+//        int[] value = {1, 2, 3};
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        input.nextLine();
+        String s = input.nextLine();
+        String[] packages = s.split(" ");
+        int[] price = new int[packages.length];
+        int[] value = new int[packages.length];
+        for (int i = 0; i < packages.length; i++){
+            price[i] = input.nextInt();
+        }
+        for (int i = 0; i < packages.length; i++){
+            value[i] = input.nextInt();
+        }
+
+        int[][] packageArray = new int[packages.length][];
+        for (int i = 0; i < packages.length; i++) {
+            String[] tmp = packages[i].split(",");
+            packageArray[i] = new int[tmp.length];
+            for (int j = 0; j < tmp.length; j++) {
+                packageArray[i][j] = Integer.parseInt(tmp[j]);
+            }
+        }
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < packageArray.length; i++) {
+            for (int j = 0; j < packageArray[i].length; j++) {
+                hashMap.put(packageArray[i][j], 1);
+            }
+        }
+        if (hashMap.size() < value.length) System.out.println("-1");
+//        System.out.println(Arrays.toString(packageArray[0]));
+//        int minNum = 1;
+//        HashMap<Integer, Integer> hashMap = new HashMap<>();
+//        for (int i = 0; i < price.length; i++) {
+//            hashMap.put(price[i], i);
+//        }
+//        Arrays.sort(price);
+//        System.out.println(Arrays.toString(price));
+//        for (int i = 1; i <= n; i++) {
+//            int min = 0;
+//            for (int j = 0; j < i; j++){
+//                min += price[i];
+//            }
+//
+//        }
+    }
+}
